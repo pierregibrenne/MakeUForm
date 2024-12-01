943047c62a3f4e22a9df9d2261cd260f1360b06f
pierregibrenne <pierre.gibrenne@protonmail.com>
2024-12-01 17:08:12 +0100

    Refactor _auth.dashboard.start route component: Add authentication check and redirect to login if not authenticated

--- src/routes/_auth.dashboard.start.tsx
+++ src/routes/_auth.dashboard.start.tsx
@@ -1,13 +1,17 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { useAuth } from '../context/useAuth'
+import { createFileRoute } from '@tanstack/react-router';
+import { useAuth } from '../context/useAuth';
+import { Navigate } from '@tanstack/react-router';
 import FormBuilder from '@/features/MakeForm/FormBuilder';
-import Login from '@/features/auth/login/Login';
+
 export const Route = createFileRoute('/_auth/dashboard/start')({
-    component: () => {
-      const { isAuthenticated } = useAuth();
-      if (!isAuthenticated) {
-        return <Login />; // Affichez un formulaire de connexion ici
-      }
-      return <div><FormBuilder/></div>;
-    },
-  });   
+  component: () => {
+    const { isAuthenticated } = useAuth();
+
+    if (!isAuthenticated) {
+      // Rediriger vers la page de login
+      return <Navigate to="/login" />;
+    }
+
+    return <div><FormBuilder /></div>;
+  },
+});
